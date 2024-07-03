6496476ab11b67ad7ac50aafd93e83c3b6011c8c
forest <859829730@qq.com>
2024-07-03 08:07:17 +0800

    素数更新

--- code/algorithm/数论/01_primer_number.cpp
+++ code/algorithm/数论/01_primer_number.cpp
@@ -1,20 +1,27 @@
 #include <cmath>
 
-// 暴力求解
+// 1.1 素数判断
 bool isPrimerNumber(int N)
 {
+    // 边界值判断
     if (N <= 1)
         return false;
     if (N == 2) 
         return true;
     
+    // 遍历[2~sqrt(N)]，搜索N的因子
     int max_x = std::sqrt(N) + 1;
     for (int x = 2; x <= max_x; ++x)
     {
+        // 存在因子，N是合数
         if (N % x == 0)
         {
             return false;
         }
     }
+
+    // N不存在因子，N是素数
     return true;
 }
+
+// 1.2 埃氏筛
